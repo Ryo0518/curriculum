ad6fa50cf8091ae7a1bebbe6bb73b47b3d67f09c
Ryo0518 <kiki.1989r@gmail.com>
2021-12-11 18:35:54 +0900

    Java
    ２章mainメソッド・インスタンス・メソッドの呼び出し講座の課題提出

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,15 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+        // 変数名:「user」
+		User user = new User(null, 0, null);
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+			user.printAccountInfo();
+
+	}
+
+}
